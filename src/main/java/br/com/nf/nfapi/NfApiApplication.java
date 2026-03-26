a3296d93cf52ff974744df27515d320821743394
Your Name <your.email@example.com>
2026-03-26 14:52:49 -0300

    Versão 2

--- src/main/java/br/com/nf/nfapi/NfApiApplication.java
+++ src/main/java/br/com/nf/nfapi/NfApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class npNfApiApplication {
+public class NfApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NfApiApplication.class, args);
