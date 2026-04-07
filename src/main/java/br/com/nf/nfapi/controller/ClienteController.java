13f8263a276fd3a4fad52da8d330cfe16a025ae3
Your Name <your.email@example.com>
2026-04-07 09:23:56 -0300

    Versão 3

--- src/main/java/br/com/nf/nfapi/controller/ClienteController.java
+++ src/main/java/br/com/nf/nfapi/controller/ClienteController.java
@@ -4,8 +4,8 @@ import br.com.nf.nfapi.entity.Cliente;
 import br.com.nf.nfapi.repository.ClienteRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
@@ -26,5 +26,28 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        cliente.setCodigo(clienteAtualizado.getCodigo());
+        cliente.setNome(clienteAtualizado.getNome());
+
+        return clienteRepository.save(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
     }
 }
