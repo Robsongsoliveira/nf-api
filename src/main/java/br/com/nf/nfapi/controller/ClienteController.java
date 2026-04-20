7eab40a3ef97e48629417630927230a48474196e
Your Name <your.email@example.com>
2026-04-20 11:42:53 -0300

    Versão 3 - Troca dentro do cliente por Repository

--- src/main/java/br/com/nf/nfapi/controller/ClienteController.java
+++ src/main/java/br/com/nf/nfapi/controller/ClienteController.java
@@ -1,7 +1,7 @@
 package br.com.nf.nfapi.controller;
 
 import br.com.nf.nfapi.entity.Cliente;
-import br.com.nf.nfapi.repository.ClienteRepository;
+import br.com.nf.nfapi.service.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,43 +11,39 @@ import java.util.List;
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    private final ClienteRepository clienteRepository;
+  //  private final ClienteRepository clienteRepository;
 
-    public ClienteController(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
+    private ClienteService clienteService;
+
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente criar(@RequestBody Cliente cliente) {
-        return clienteRepository.save(cliente);
+        return clienteService.criar(cliente);
     }
 
     @GetMapping
     public List<Cliente> listar() {
-        return clienteRepository.findAll();
+        return clienteService.listar();
     }
 
     @GetMapping("/{id}")
     public Cliente buscarPorId(@PathVariable Long id) {
-        return clienteRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        return clienteService.buscarPorId(id);
     }
 
     @PutMapping("/{id}")
     public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado) {
-        Cliente cliente = clienteRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-
-        cliente.setCodigo(clienteAtualizado.getCodigo());
-        cliente.setNome(clienteAtualizado.getNome());
-
-        return clienteRepository.save(cliente);
+        return clienteService.atualizar(id, clienteAtualizado);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id) {
-        clienteRepository.deleteById(id);
+        clienteService.excluir(id);
     }
 }
